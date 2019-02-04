83dddd863f3f066ba929081168be23a841d2a6d6
Vishesh51 <visheshsharma51@gmail.com>
2019-02-03 18:19:39 -0800

    Add files via upload

--- del.py
+++ del.py
@@ -5,6 +5,6 @@ test = os.listdir(dir_name)
 
 """for item in test:
 	dir1=os.listdir(dir_name+item)"""
-for item1 in dir1:
-	if item1.endswith(".pgm"):
+for item in dir1:
+	if item.endswith(".pgm"):
 		os.remove(os.path.join(dir_name, item))
